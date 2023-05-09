f271e576d48dbf6f575268739b66d6cc6b4938a4
MaickDavila <librajunior13@gmail.com>
2023-05-09 09:18:37 -0500

    home layout

--- src/components/layouts/defaultLayout/components/defaultNavbar.vue
+++ src/components/layouts/defaultLayout/components/defaultNavbar.vue
@@ -1,7 +1,11 @@
+<script setup lang="ts">
+import logout from "../../../auth/actions/logout";
+</script>
+
 <template>
   <div class="w-full shadow-md h-16 flex items-center justify-between bg-white">
     <div></div>
-    <el-button type="text" class="mr-3">
+    <el-button @click="logout" type="text" class="mr-3">
       <i class="fa-solid fa-right-from-bracket text-xl"></i>
     </el-button>
   </div>
