2435ca9d9b814cbd2ca6110fb23d77320009dae3
Carlos Alberto Casado Vargas <carloscasadovargas@gmail.com>
2023-05-10 18:26:29 -0500

    remove ref import

--- src/components/auth/components/login/loginForm.vue
+++ src/components/auth/components/login/loginForm.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-  import { ref } from "vue";
   import { UserFilled, Lock } from "@element-plus/icons-vue";
   const inputUsername = ref("");
   const inputPassword = ref("");
